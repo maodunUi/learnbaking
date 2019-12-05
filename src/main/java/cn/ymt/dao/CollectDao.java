f1e4b0a076482055df684516a1307d7bc17ac76e
maodunUi <2458009530@qq.com>
2019-12-05 14:04:45 +0800

    1028_2更新数据库

--- src/main/java/cn/ymt/dao/CollectDao.java
+++ src/main/java/cn/ymt/dao/CollectDao.java
@@ -1,6 +1,11 @@
 package cn.ymt.dao;
 import cn.ymt.view.CollectView;
 import cn.ymt.query.CollectQueryParams;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface CollectDao extends DaoTemplate<CollectView,CollectQueryParams>{
-  
+   List<CollectView> getMyCollectDetail(@Param("id") Integer id) throws Exception;
+   List<CollectView> getMyCollectCourse(@Param("id") Integer id) throws Exception;
 }
