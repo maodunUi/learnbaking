e16b0be236f19741942dc1d8b4a6fc122b2def6d
maodunUi <2458009530@qq.com>
2019-12-05 14:04:35 +0800

    1028_2更新数据库

---
+++ src/main/java/cn/ymt/controller/AuthController.java
@@ -0,0 +1,16 @@
+package cn.ymt.controller;
+
+import cn.ymt.util.jsonResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/AuthController")
+public class AuthController {
+    @RequestMapping("/auth")
+    public jsonResult auth(HttpServletRequest request){
+        return new jsonResult(false,"用户未登录") ;
+    }
+}
--- src/main/java/cn/ymt/controller/CollectController.java
+++ src/main/java/cn/ymt/controller/CollectController.java
@@ -1,11 +1,68 @@
 package cn.ymt.controller;
+import cn.ymt.pojo.Collect;
+import cn.ymt.pojo.User;
+import cn.ymt.util.jsonResult;
 import cn.ymt.view.CollectView;
 import cn.ymt.serviceDao.CollectServiceDao;
 import cn.ymt.query.CollectQueryParams;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@CrossOrigin
+@RestController
 @RequestMapping("/CollectAction")
 public class CollectController  {
-
+    @Autowired
+    private CollectServiceDao collectServiceDao ;
+    //得到用户自己的收藏
+    @RequestMapping("/getCollect")
+    public jsonResult getCollect(HttpServletRequest request){
+        User user = (User) request.getSession().getAttribute("user");
+        try {
+            List<CollectView> details = collectServiceDao.getMyCollectDetail(user.getId()) ;
+            List<CollectView> collectViews = collectServiceDao.getMyCollectCourse(user.getId()) ;
+            List<Object> lists = new ArrayList<>() ;
+            lists.add(details) ;
+            lists.add(collectViews) ;
+            long l = details.size() + collectViews.size() ;
+            return new jsonResult(true,"查询成功",l,lists) ;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new jsonResult(false,"失败") ;
+        }
+    }
+    //用户收藏  type courseId[detailId]
+    //0:detailId 1:courseId
+    @RequestMapping("/collect")
+    public jsonResult collect(CollectView collect,HttpServletRequest request){
+        User user = (User) request.getSession().getAttribute("user");
+        collect.setUserId(user.getId());
+        collect.setCollectTime(new Date());
+        try {
+            collectServiceDao.insert(collect) ;
+            return new jsonResult(true,"收藏成功") ;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new jsonResult(false,"收藏失败") ;
+        }
+    }
+    //取消收藏 id collect的id
+    @RequestMapping("/cancel")
+    public jsonResult cancel(Integer id){
+        try {
+            collectServiceDao.deleteById(id) ;
+            return new jsonResult(true,"取消收藏成功") ;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new jsonResult(false,"取消收藏失败") ;
+        }
+    }
 }
