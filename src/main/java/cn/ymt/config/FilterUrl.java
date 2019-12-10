8f24464567db8f6f0dd54cb23333f0dd847c3248
maodunUi <2458009530@qq.com>
2019-12-10 09:28:45 +0800

    1028_2更新数据库

--- src/main/java/cn/ymt/config/FilterUrl.java
+++ src/main/java/cn/ymt/config/FilterUrl.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class FilterUrl {
     private static String[] strArrays  = {
             "/test","/CollectAction/auth","/CollectAction/getCollect","/WorkAction/getMyself","/ShopcartAction/addToCart","/UserAction/get","/DetailAction/update",
-            "CollectAction/collect"
+            "/CollectAction/collect"
     } ;
     public static List<String> getFilterUrl(){
         return Arrays.asList(FilterUrl.strArrays) ;
