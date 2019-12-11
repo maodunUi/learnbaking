3776d3f006025392ef255e95d1b33e59f7e19242
maodunUi <2458009530@qq.com>
2019-12-11 11:42:48 +0800

    1028_2更新数据库

--- src/main/java/cn/ymt/controller/UserlikeController.java
+++ src/main/java/cn/ymt/controller/UserlikeController.java
@@ -1,11 +1,31 @@
 package cn.ymt.controller;
+import cn.ymt.pojo.User;
+import cn.ymt.util.jsonResult;
 import cn.ymt.view.UserlikeView;
 import cn.ymt.serviceDao.UserlikeServiceDao;
 import cn.ymt.query.UserlikeQueryParams;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/UserlikeAction")
 public class UserlikeController  {
-
+@Autowired
+    private UserlikeServiceDao userlikeServiceDao ;
+//用户点赞 传workId
+@RequestMapping("/like")
+public jsonResult like(UserlikeView userlikeView,HttpServletRequest request){
+        User u = (User) request.getSession().getAttribute("user");
+        userlikeView.setUserId(u.getId());
+    try {
+        userlikeServiceDao.insert(userlikeView) ;
+        return new jsonResult(true,userlikeView.getId()+"") ;
+    } catch (Exception e) {
+        e.printStackTrace();
+        return new jsonResult(false,"点赞失败") ;
+    }
+}
 }
