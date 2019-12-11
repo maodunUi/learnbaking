9dddbc7f85891556a000a674fc4abd4bba210bbd
maodunUi <2458009530@qq.com>
2019-12-11 12:05:05 +0800

    1028_2更新数据库

--- src/main/java/cn/ymt/controller/UserlikeController.java
+++ src/main/java/cn/ymt/controller/UserlikeController.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping("/UserlikeAction")
 public class UserlikeController  {
 @Autowired
