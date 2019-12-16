27bf891782718fbf4b2948349e79fb5b42759bf8
maodunUi <2458009530@qq.com>
2019-12-16 18:41:48 +0800

    1028_2更新数据库

---
+++ src/main/java/cn/ymt/controller/PayControllerApp.java
@@ -0,0 +1,191 @@
+package cn.ymt.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.ymt.pojo.User;
+import cn.ymt.serviceDao.OrdersServiceDao;
+import cn.ymt.serviceDao.ShopcartServiceDao;
+import cn.ymt.util.AlipayConfig;
+import cn.ymt.view.OrdersView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayTradeWapPayModel;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayTradeWapPayRequest;
+
+
+@Controller
+@RequestMapping("/payApp")
+public class PayControllerApp {
+	@Autowired
+	private OrdersServiceDao orderServiceDao ;
+	@Autowired
+	private ShopcartServiceDao shopcartServiceDao ;
+
+
+	@RequestMapping(value = "/pay", method = { RequestMethod.GET, RequestMethod.POST })
+	public void pay(HttpServletRequest request, HttpServletResponse response) throws IOException, AlipayApiException {
+
+			// 商户订单号，商户网站订单系统中唯一订单号，必填
+			String out_trade_no = UUID.randomUUID().toString() ;
+			// 订单名称，必填
+			String subject = new String(request.getParameter("subject").getBytes("ISO-8859-1"), "UTF-8");
+			// 付款金额，必填
+			String total_amount = new String(request.getParameter("totalPrice").getBytes("ISO-8859-1"), "UTF-8");
+			// 商品描述，可空
+			String body = new String(request.getParameter("body").getBytes("ISO-8859-1"), "UTF-8");
+			// 超时时间 可空
+			String timeout_express = "2m";
+			// 销售产品码 必填
+			String product_code = "QUICK_WAP_WAY";
+			/**********************/
+			// SDK 公共请求类，包含公共请求参数，以及封装了签名与验签，开发者无需关注签名与验签
+			// 调用RSA签名方式
+			AlipayClient client = new DefaultAlipayClient(AlipayConfig.gatewayUrl, AlipayConfig.app_id,
+					AlipayConfig.merchant_private_key, "json", AlipayConfig.charset, AlipayConfig.alipay_public_key,
+					AlipayConfig.sign_type);
+			AlipayTradeWapPayRequest alipay_request = new AlipayTradeWapPayRequest();
+
+			// 封装请求支付信息
+			AlipayTradeWapPayModel model = new AlipayTradeWapPayModel();
+			model.setOutTradeNo(out_trade_no);
+			model.setSubject("订单");
+			model.setTotalAmount(total_amount);
+			model.setBody(body);
+			model.setTimeoutExpress(timeout_express);
+			model.setProductCode(product_code);
+			alipay_request.setBizModel(model);
+			// 设置异步通知地址
+			alipay_request.setNotifyUrl(AlipayConfig.notify_url);
+			// 设置同步地址
+			alipay_request.setReturnUrl(AlipayConfig.return_url);
+
+			String callback = request.getParameter("callback");
+
+
+		User user = (User) request.getSession().getAttribute("user");
+		OrdersView orderView = new OrdersView() ;
+		orderView.setOrderTime(new Date());
+		orderView.setCourseId(Integer.parseInt(request.getParameter("courseId")));
+		orderView.setUserId(user.getId());
+		orderView.setPrice(Double.valueOf(total_amount));
+		orderView.setSubject(body);
+		orderView.setName(subject);
+		orderView.setState((byte)0); //刚刚生成订单，未支付
+		try {
+			orderServiceDao.insert(orderView) ;
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.getWriter().print("订单生成失败" + e.getMessage());
+		}
+
+			// form表单生产
+			String form = "";
+			try {
+				// 调用SDK生成表单
+				form = client.pageExecute(alipay_request).getBody();
+
+				response.setContentType("text/html;charset=" + AlipayConfig.charset);
+				response.getWriter().write(form);// 直接将完整的表单html输出到页面
+				response.getWriter().flush();
+				response.getWriter().close();
+			} catch (AlipayApiException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+	@RequestMapping(value = "/return_url", method = { RequestMethod.GET, RequestMethod.POST })
+	public void return_url(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 获取支付宝GET过来反馈信息
+		PrintWriter out = response.getWriter();
+		Map<String, String> params = new HashMap<String, String>();
+		Map requestParams = request.getParameterMap();
+		for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+			String name = (String) iter.next();
+			String[] values = (String[]) requestParams.get(name);
+			String valueStr = "";
+			for (int i = 0; i < values.length; i++) {
+				valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+			}
+			// 乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+			valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+			params.put(name, valueStr);
+		}
+
+		// 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+		// 商户订单号
+		String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+		// 支付宝交易号
+		String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+
+		// 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+		// 计算得出通知验证结果
+		// boolean AlipaySignature.rsaCheckV1(Map<String, String> params, String
+		// publicKey, String charset, String sign_type)
+		boolean verify_result = AlipaySignature.rsaCheckV1(params, AlipayConfig.alipay_public_key, AlipayConfig.charset,
+				"RSA2");
+
+		if (verify_result) {// 验证成功
+			OrdersView userorder = orderServiceDao.getByNumber(out_trade_no); // 通过订单号找到订单
+			userorder.setState((byte)1); //订单支付成功
+			orderServiceDao.updateNotNullField(userorder); // 更新订单表
+
+			StringBuffer sf = new StringBuffer();
+			sf.append("<script type=\"text/javascript\">");
+			sf.append("window.location.href = 'http://127.0.0.1:3000/#/home");
+			sf.append("</script>");
+			String str = sf.toString();
+			out.println(str);
+			// out.println(params) ;
+
+			// ——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+		} else {
+			// 该页面可做页面美工编辑
+			// out.clear();
+			out.println(false);
+		}
+	}
+
+	@RequestMapping(value = "/tests", method = { RequestMethod.GET, RequestMethod.POST })
+	public void tests(HttpServletResponse response) throws IOException {
+		StringBuffer sf = new StringBuffer();
+		// sf.append("<html><head><meta http-equiv=\"Content-Type\"
+		// content=\"text/html;"+"\"/></head><body>");
+		// sf.append("<form id = \"pay_form\" action=\"" + reqUrl
+		// + "\" method=\"post\">");
+		/*
+		 * if (null != hiddens && 0 != hiddens.size()) { Set<Entry<String,
+		 * String>> set = hiddens.entrySet(); Iterator<Entry<String, String>> it
+		 * = set.iterator(); while (it.hasNext()) { Entry<String, String> ey =
+		 * it.next(); String key = ey.getKey(); String value = ey.getValue();
+		 * sf.append("<input type=\"hidden\" name=\"" + key + "\" id=\"" + key +
+		 * "\" value=\"" + value + "\"/>");
+		 * 
+		 * <script> window.location.href =
+		 * 'http://127.0.0.1:5501/mall/www/index.html#/tab/home' </script> } }
+		 */
+		// sf.append("</form>");
+		// sf.append("</body>");
+		sf.append("<script type=\"text/javascript\">");
+		// sf.append("document.all.pay_form.submit();");
+		sf.append("window.location.href = 'http://127.0.0.1:5501/mall/www/index.html#/tab/home'");
+		sf.append("</script>");
+		// sf.append("</html>");
+		String str = sf.toString();
+
+		response.getWriter().println(sf);
+	}
+}
--- src/main/java/cn/ymt/util/AlipayConfig.java
+++ src/main/java/cn/ymt/util/AlipayConfig.java
@@ -1,4 +1,4 @@
-package cn.ymt.util;
+﻿package cn.ymt.util;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,47 +14,47 @@ import java.io.IOException;
  */
 
 public class AlipayConfig {
-
+	
 //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 	// 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
 	public static String app_id = "2016093000633301";
-
+	
 	// 商户私钥，您的PKCS8格式RSA2私钥
-	public static String merchant_private_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCRUpY6xZYig2xyjdcZvq5xbZYANNfBaOn2jcF9cPr6iz0CsU9INKLc7"
-			+ "0SvjXX6Mw0u0jOLVmdR2aeRaZLvwhX/tHTD0SB0Biaq5GdDLxgMeqdkN9Bp40RyPxqxNZigiPxmnUiQYs1Br3gW4RZW6z0CUeKf+i+br+EJAq6a6VqmZ/ZKXCxUKhllaqXkBfDhAMQ/LDNRV"
-			+ "wqz2LwsF2wcHPgDE0JZE67+oOL6/iVVLjgcLFKFm4p5zEGu8PVwSHHpV04bM1UTfKTXrJdVAE04JqWqb48Ugi0nlht/QpEvUMrZLruskyobo4vo856hhTOSjJONPGV2b9qqu2zdj5C5sqpnAg"
-			+ "MBAAECggEAT1vnWE8AbvaQKXqV5aRAxQaQLKquzBsL8S12ZZdmJ54UAj1ry0WbTsWHH8tDiw+H+nKl3UihAYy3umwJ5s55jvsv7KNuco3NXfN/BKxaGK5+bPch5EFM5orZe76GC9zi4PKxZs/d"
-			+ "wSoejXf76BlsZrDq7mwExaNlY7NIQKaAb3BxnoxzxJAdZlHF2Wl1GLeKMyuFSxDWNbWoAFX6v9kveik7/n2Dc56kUw5K0XyTTnI5ZKqFKqEf0g2kHOW39bLWf01GYqqrxfqlkOpl/fT89mffYouBx"
-			+ "lJha/+vq4LSTSh5J9dtggBLXg6d4C1NwYim2LbhAaord41YySlyRYnCsQKBgQDKq+Y/wARGrWnvpdFQga5oRCELqIAphfvQLP1aFJWBVpJJs8aT1/SdiHV6aMUR3UztaXaKWTPbXaQUFVxJ2befAym"
-			+ "4M4Ez9PkBt51HaxnsJeH4WTPZeMwfdWa4QlpotAFG+t7ARlCQD9ZwXJxZ9kKnG8fLoMBPUpbSQvEcWADj7wKBgQC3j5/ugLTl/jrkurAKkn1ve10nbSr2KYlOEI4tio1BVMfxTS+8caSrAiQVI/cueo"
-			+ "ScGm+B+a5DZv/TnD7Ga0qXDTH3HjhpCLWw0FLmfyrnZtVZfACbW395Ip8vUY8oxVfBs5Tx2F9ta+bL88HQ/9lVBJhrDMqcRwxub//gOIzJCQKBgQCRYVup0TCADncWTAjoyg10h6J4vWcp4g1vGEaobmm"
-			+ "MPLB1I/tHJDs5IaC7rRnoJanAB83bkf9L1VEUgvoPk5hO/TLZCkEdJxXTsIWmrGIM7mO5BSvXTPqHemyzsg0uXCkqZFyS6h1yKZo2QYh6U9ceDiR0ohwxClpb+17/aPVCiwKBgCTQkWrsNVvLWVR2fMgiCTZ"
-			+ "Kr6wwoOUt/3PArkND/oCT2GX07RYGVNhXXZU/2VthByenxaVzxheOmNCADbcN3a/d3Z+KWBX4wTftEDNaXeh7H18LF7QQ7cgFwZdj7CppKeiXc8/tbXYspeVRxhV8usHqYgdWUWx2c2/S1qbJsLLBAoGAFR0"
-			+ "N7+l6yiEyyACvZxXwfJ97BXEUzaip+Ibs"
-			+ "IdrwKGyvcbTkufI2uZ4HyVbczwNEG92mk1Jbqc/+P1/mbp6XNOwMf9d+//ic39MDYCa79+RRE6WD4NOB/QF+3K/T8eGUAMJZr3FqXKqJyKC7/wJ8PPLOzpYBkpu3HXcfhZmcshE=";
-
+    public static String merchant_private_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCRUpY6xZYig2xyjdcZvq5xbZYANNfBaOn2jcF9cPr6iz0CsU9INKLc7"
+    		+ "0SvjXX6Mw0u0jOLVmdR2aeRaZLvwhX/tHTD0SB0Biaq5GdDLxgMeqdkN9Bp40RyPxqxNZigiPxmnUiQYs1Br3gW4RZW6z0CUeKf+i+br+EJAq6a6VqmZ/ZKXCxUKhllaqXkBfDhAMQ/LDNRV"
+    		+ "wqz2LwsF2wcHPgDE0JZE67+oOL6/iVVLjgcLFKFm4p5zEGu8PVwSHHpV04bM1UTfKTXrJdVAE04JqWqb48Ugi0nlht/QpEvUMrZLruskyobo4vo856hhTOSjJONPGV2b9qqu2zdj5C5sqpnAg"
+    		+ "MBAAECggEAT1vnWE8AbvaQKXqV5aRAxQaQLKquzBsL8S12ZZdmJ54UAj1ry0WbTsWHH8tDiw+H+nKl3UihAYy3umwJ5s55jvsv7KNuco3NXfN/BKxaGK5+bPch5EFM5orZe76GC9zi4PKxZs/d"
+    		+ "wSoejXf76BlsZrDq7mwExaNlY7NIQKaAb3BxnoxzxJAdZlHF2Wl1GLeKMyuFSxDWNbWoAFX6v9kveik7/n2Dc56kUw5K0XyTTnI5ZKqFKqEf0g2kHOW39bLWf01GYqqrxfqlkOpl/fT89mffYouBx"
+    		+ "lJha/+vq4LSTSh5J9dtggBLXg6d4C1NwYim2LbhAaord41YySlyRYnCsQKBgQDKq+Y/wARGrWnvpdFQga5oRCELqIAphfvQLP1aFJWBVpJJs8aT1/SdiHV6aMUR3UztaXaKWTPbXaQUFVxJ2befAym"
+    		+ "4M4Ez9PkBt51HaxnsJeH4WTPZeMwfdWa4QlpotAFG+t7ARlCQD9ZwXJxZ9kKnG8fLoMBPUpbSQvEcWADj7wKBgQC3j5/ugLTl/jrkurAKkn1ve10nbSr2KYlOEI4tio1BVMfxTS+8caSrAiQVI/cueo"
+    		+ "ScGm+B+a5DZv/TnD7Ga0qXDTH3HjhpCLWw0FLmfyrnZtVZfACbW395Ip8vUY8oxVfBs5Tx2F9ta+bL88HQ/9lVBJhrDMqcRwxub//gOIzJCQKBgQCRYVup0TCADncWTAjoyg10h6J4vWcp4g1vGEaobmm"
+    		+ "MPLB1I/tHJDs5IaC7rRnoJanAB83bkf9L1VEUgvoPk5hO/TLZCkEdJxXTsIWmrGIM7mO5BSvXTPqHemyzsg0uXCkqZFyS6h1yKZo2QYh6U9ceDiR0ohwxClpb+17/aPVCiwKBgCTQkWrsNVvLWVR2fMgiCTZ"
+    		+ "Kr6wwoOUt/3PArkND/oCT2GX07RYGVNhXXZU/2VthByenxaVzxheOmNCADbcN3a/d3Z+KWBX4wTftEDNaXeh7H18LF7QQ7cgFwZdj7CppKeiXc8/tbXYspeVRxhV8usHqYgdWUWx2c2/S1qbJsLLBAoGAFR0"
+    		+ "N7+l6yiEyyACvZxXwfJ97BXEUzaip+Ibs"
+    		+ "IdrwKGyvcbTkufI2uZ4HyVbczwNEG92mk1Jbqc/+P1/mbp6XNOwMf9d+//ic39MDYCa79+RRE6WD4NOB/QF+3K/T8eGUAMJZr3FqXKqJyKC7/wJ8PPLOzpYBkpu3HXcfhZmcshE=";
+	
 	// 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-	public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgR5C0KpxDf/062p2vFO5fF4/1BPcm8hhkORDdnkdOtI5rnqexJ4vdLiC4if0urH65MBlJkwO27"
-			+ "IV24mOaQ6vUm6j08oV1iDzBvPkc2HQqoaUA/O+XW8Mpl/rlKbUFuZ3k/jLsKe9JqWrH5uvIJLvaGW6smooYJcXKQYWE1wD0OcPx9xTeo1qrhYpkZCfBUFyW2RRvSKXvHIOdN5YBYh+BWbIgST9S3JZNbmwGYZnzm"
-			+ "Nz9l0JjH8CnaToGxly9lo468YFanDm11r5IFXpQKwoscUZyeDGL3hxOlDfNXdDm/uQpEpkMtd0K0LLdCTY2JZQsY36ZVIBzXL1a60X49UzkwIDAQAB";
+    public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgR5C0KpxDf/062p2vFO5fF4/1BPcm8hhkORDdnkdOtI5rnqexJ4vdLiC4if0urH65MBlJkwO27"
+    		+ "IV24mOaQ6vUm6j08oV1iDzBvPkc2HQqoaUA/O+XW8Mpl/rlKbUFuZ3k/jLsKe9JqWrH5uvIJLvaGW6smooYJcXKQYWE1wD0OcPx9xTeo1qrhYpkZCfBUFyW2RRvSKXvHIOdN5YBYh+BWbIgST9S3JZNbmwGYZnzm"
+    		+ "Nz9l0JjH8CnaToGxly9lo468YFanDm11r5IFXpQKwoscUZyeDGL3hxOlDfNXdDm/uQpEpkMtd0K0LLdCTY2JZQsY36ZVIBzXL1a60X49UzkwIDAQAB";
 
 	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
 	public static String notify_url = "http://localhost:8080/AliPay/notify_url.jsp";
 
 	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
 	//public static String return_url = "http://localhost:8080/AliPay/return_url.jsp";
-	public static String return_url = "http://127.0.0.1:8080/pay/return_url";
+	public static String return_url = "http://127.0.0.1:8091/payApp/return_url";
 
 	// 签名方式
 	public static String sign_type = "RSA2";
-
+	
 	// 字符编码格式
 	public static String charset = "utf-8";
-
+	
 	// 支付宝网关
 	public static String gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
-
+	
 	public static String FORMAT = "json";
 	// 支付宝网关
 	public static String log_path = "C:\\";
@@ -62,26 +62,26 @@ public class AlipayConfig {
 
 //↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
-	/**
-	 * 写日志，方便测试（看网站需求，也可以改成把记录存入数据库）
-	 * @param sWord 要写入日志里的文本内容
-	 */
-	public static void logResult(String sWord) {
-		FileWriter writer = null;
-		try {
-			writer = new FileWriter(log_path + "alipay_log_" + System.currentTimeMillis()+".txt");
-			writer.write(sWord);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (writer != null) {
-				try {
-					writer.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    /** 
+     * 写日志，方便测试（看网站需求，也可以改成把记录存入数据库）
+     * @param sWord 要写入日志里的文本内容
+     */
+    public static void logResult(String sWord) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(log_path + "alipay_log_" + System.currentTimeMillis()+".txt");
+            writer.write(sWord);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
 
