5b38ef749a7975560a27d457c8767738abb9d208
maodunUi <2458009530@qq.com>
2019-12-17 10:21:53 +0800

    1028_2更新数据库

--- src/main/java/cn/ymt/controller/CommentController.java
+++ src/main/java/cn/ymt/controller/CommentController.java
@@ -12,12 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping("/CommentAction")
 public class CommentController  {
 
